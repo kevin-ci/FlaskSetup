7966002a7b5920b2433f6e82d056d6280e8f4092
kevin-ci <kevin@codeinstitute.net>
2020-03-26 09:05:12 +0000

    test

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
+import os
+from os import path
+if path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
+
+#print(os.environ.get("SECRET_KEY"))
+app.secret_key = "test"
 
 @app.route('/')
 def home():
     return render_template('hello.html')
-
-@app.route('/test')
-def test():
-    return render_template('testing.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
