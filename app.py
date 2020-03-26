be6c6e116ee7446195f96b3b842b10603f8287ae
kevin-ci <kevin@codeinstitute.net>
2020-03-26 09:16:40 +0000

    heroku

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template
 import os
 from os import path
-if path.exists("env.py"):
-    import env
 
 app = Flask(__name__)
 
-#print(os.environ.get("SECRET_KEY"))
-app.secret_key = "test"
-
 @app.route('/')
 def home():
-    return render_template('hello.html')
+    return "Hello World"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
