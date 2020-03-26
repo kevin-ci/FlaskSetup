04c6dc3ba80dc344557af3ee8baf982d7196c7f1
kevin-ci <kevin@codeinstitute.net>
2020-03-26 09:12:19 +0000

    port

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app.secret_key = "test"
 def home():
     return render_template('hello.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-    port=5000,
-    debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
